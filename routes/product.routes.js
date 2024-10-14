320af0cc0bc552e22f84d9bcab9b9a2b116b297b
Devang18 <137139535+devangrana18@users.noreply.github.com>
2024-10-14 15:13:09 +0530

    product.routes updated

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -13,32 +13,32 @@ router.get('/:category', async (req, res) => {
       res.status(500).json({ message: error.message });
   }
 });
-// router.post('/', async (req, res) => {
-//   const product = new Product({
-//     name: req.body.name,
-//     price: req.body.price,
-//     discountedPrice:req.body.discountedPrice,
-//     description: req.body.description,
-//     imageUrl: req.body.imageUrl,
-//     category:req.body.category,
-//     brand:req.body.brand,
-//     stock:req.body.stock
-//   });
-//   try {
-//     const newProduct = await product.insertMany();
-//     res.status(201).json(newProduct);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
 router.post('/', async (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+    discountedPrice:req.body.discountedPrice,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    category:req.body.category,
+    brand:req.body.brand,
+    stock:req.body.stock
+  });
   try {
-    const products = await Product.insertMany(req.body);
-    res.status(201).json(products);
+    const newProduct = await product.insertMany();
+    res.status(201).json(newProduct);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
+// router.post('/', async (req, res) => {
+//   try {
+//     const products = await Product.insertMany(req.body);
+//     res.status(201).json(products);
+//   } catch (err) {
+//     res.status(400).json({ message: err.message });
+//   }
+// });
 
 
 
